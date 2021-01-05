3fc9fd8a4a94db1079acde455e3a3d4d8f274ca0
derek pick <derek.pick@ifelse.ch>
2021-01-05 22:50:23 +0100

    Unit Tests for Command Model

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests
+    {
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+            var testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Arrange
+            var testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+
+            //Act
+            testCommand.Platform = "Linux";
+
+            //Assert
+            Assert.Equal("Linux", testCommand.Platform); 
+        }
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+            var testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+
+            //Act
+            testCommand.CommandLine = "dotnet run";
+
+            //Assert
+            Assert.Equal("dotnet run", testCommand.CommandLine); 
+        }
+    }
+}
